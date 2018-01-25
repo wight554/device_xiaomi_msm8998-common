7216ad34708676d2aa91940a983bcb411dd707bf
Dan Pasanen <dan.pasanen@gmail.com>
2018-01-25 15:22:52 -0600

    msm8998-common: build 32-bit android.frameworks.displayservice@1.0
    * Needed for camera hal
    
    Change-Id: Ifc1234ac16f1791e227b7a1fc5d17fe5df336592

--- msm8998.mk
+++ msm8998.mk
@@ -136,6 +136,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
