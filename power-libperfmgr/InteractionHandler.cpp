68d6571210a0fc3cc0a90f7814db448432b95311
Michael Wright <michaelwr@google.com>
2018-06-21 02:38:28 +0100

    msm8998-common: Update power HAL to version 1.3.
    This adds support for the EXPENSIVE_RENDERING power hint.
    
    BUG: 110112323
    Test: adb shell cat /sys/class/kgsl/kgsl-3d0/devfreq/cur_freq
          Verified GPU frequency reset by adb shell pkill surfaceflinger and system_server
    Change-Id: Ia1f6e99b67c5e88b610412ad30f152d241095a48
    Merged-In: Ia1f6e99b67c5e88b610412ad30f152d241095a48

--- power-libperfmgr/InteractionHandler.cpp
+++ power-libperfmgr/InteractionHandler.cpp
@@ -16,7 +16,7 @@
 
 //#define LOG_NDEBUG 0
 
-#define LOG_TAG "android.hardware.power@1.2-service.xiaomi_msm8998-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.xiaomi_msm8998-libperfmgr"
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
 
 #include <fcntl.h>
