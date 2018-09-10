bfc42d4c55b1ed6e4ea7db219eae204a53cf1824
Vol Zhdanov <wight554@gmail.com>
2018-09-10 18:42:52 +0000

    msm8998-common: switch to power-libperfmgr
    * cleanup unused camera hints
    * revert b296bbf

--- msm8998.mk
+++ msm8998.mk
@@ -265,6 +265,13 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.2-service.xiaomi_msm8998-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Properties
 include $(LOCAL_PATH)/vendor_prop.mk
 
