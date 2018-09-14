e58f9bfeb8df38b8ff66fafee9cd351d0c6415f1
Vol Zhdanov <wight554@gmail.com>
2018-09-14 10:55:33 +0000

    msm8998-common: gps: import from LA.UM.7.2.r1-04000-sdm660.0

--- gps/android/Android.mk
+++ gps/android/Android.mk
@@ -1,11 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.gnss@1.0-impl-qti
+LOCAL_MODULE := android.hardware.gnss@1.1-impl-qti
 LOCAL_VENDOR_MODULE := true
-LOCAL_MODULE_OWNER := qti
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SRC_FILES := \
     AGnss.cpp \
     Gnss.cpp \
@@ -37,14 +35,15 @@ LOCAL_SHARED_LIBRARIES := \
     libhidlbase \
     libhidltransport \
     libhwbinder \
+    libcutils \
     libutils \
     android.hardware.gnss@1.0 \
+    android.hardware.gnss@1.1 \
 
 LOCAL_SHARED_LIBRARIES += \
     libloc_core \
     libgps.utils \
     libdl \
-    libloc_pla \
     liblocation_api \
 
 LOCAL_CFLAGS += $(GNSS_CFLAGS)
@@ -59,11 +58,10 @@ endif # BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET
 
 ifeq ($(BUILD_GNSS_HIDL_SERVICE), true)
 include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.gnss@1.0-service-qti
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_INIT_RC := android.hardware.gnss@1.0-service-qti.rc
+LOCAL_MODULE := android.hardware.gnss@1.1-service-qti
 LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_INIT_RC := android.hardware.gnss@1.1-service-qti.rc
 LOCAL_SRC_FILES := \
     service.cpp \
 
@@ -74,6 +72,7 @@ LOCAL_HEADER_LIBRARIES := \
     libloc_core_headers \
     libloc_pla_headers \
     liblocation_api_headers
+
 
 LOCAL_SHARED_LIBRARIES := \
     liblog \
@@ -87,6 +86,7 @@ LOCAL_SHARED_LIBRARIES += \
     libhidlbase \
     libhidltransport \
     android.hardware.gnss@1.0 \
+    android.hardware.gnss@1.1 \
 
 LOCAL_CFLAGS += $(GNSS_CFLAGS)
 include $(BUILD_EXECUTABLE)
