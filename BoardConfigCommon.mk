e304e7e0cd206249b93bb8f0bc7fe914a6a36f07
Vol Zhdanov <wight554@gmail.com>
2018-09-14 17:37:30 +0000

    msm8998-common: enable and fix-up some sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -202,8 +202,8 @@ TARGET_RIL_VARIANT := caf
 VENDOR_SECURITY_PATCH := 2018-07-01
 
 # SELinux
-# include device/qcom/sepolicy/sepolicy.mk
-# BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
