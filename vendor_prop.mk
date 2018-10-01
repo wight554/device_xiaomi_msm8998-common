4932a4840c92ecb515839e638f3d90a6c71c25fe
Vol Zhdanov <wight554@gmail.com>
2018-10-01 13:36:24 +0000

    msm8998-common: update memory tweaks
    * from polaris-user-9-PKQ1.180729.001-8.8.28-release-keys

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -103,12 +103,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Memory
 PRODUCT_PROPERTY_OVERRIDES += \
-	dalvik.vm.heapgrowthlimit=256m \
 	dalvik.vm.heapmaxfree=8m \
-	dalvik.vm.heapminfree=4m \
+	dalvik.vm.heapminfree=512k \
 	dalvik.vm.heapsize=512m \
-	dalvik.vm.heapstartsize=16m \
+	dalvik.vm.heapstartsize=8m \
 	dalvik.vm.heaptargetutilization=0.75
+
+# Memory (CAF)
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.vendor.qti.sys.fw.bg_apps_limit=60 \
+	ro.vendor.qti.sys.fw.bservice_enable=true \
+	sys.tp.grip_enable=0
 
 # Network manager
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -124,8 +129,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.vendor.extension_library=libqti-perfd-client.so \
-	ro.vendor.qti.sys.fw.bg_apps_limit=60
+	ro.vendor.extension_library=libqti-perfd-client.so
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
