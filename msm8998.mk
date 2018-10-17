535021198fa6f91910c6a9121e829f0454e20945
Vol Zhdanov <wight554@gmail.com>
2018-10-17 07:32:52 +0000

    Revert "msm8998-common: Disable telephony-ext for now"
    This reverts commit 12784adf8ea7667d5b3376698a2b6978747d9d65.

--- msm8998.mk
+++ msm8998.mk
@@ -331,6 +331,13 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service
 
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
