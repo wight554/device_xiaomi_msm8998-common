4acfc1cd5ca7856450dddcfa6e7494cc3d08ef93
Vol Zhdanov <wight554@gmail.com>
2018-11-17 21:43:37 +0000

    msm8998-common: update proprietary files list for pie
    * keep libqminvapi.so for readmac
    * partially keep sensors untouched
    
    * extracted from:
     * jasmine PKQ1.180904.001
     * poplar_dsds YOSHINO-2.2.0-CAF-181103-0550
     * fajita PKQ1.180716.001 (ANT+)
    
    * references:
     * https://review.lineageos.org/231332
     * https://review.lineageos.org/228374

--- extract-files.sh
+++ extract-files.sh
@@ -79,24 +79,6 @@ MI_CAMERA_HAL="$COMMON_BLOB_ROOT"/vendor/lib/libMiCameraHal.so
 sed -i "s|system/etc/dualcamera.png|vendor/etc/dualcamera.png|g" "$MI_CAMERA_HAL"
 
 #
-# Correct VZW IMS library location
-#
-QTI_VZW_IMS_INTERNAL="$COMMON_BLOB_ROOT"/vendor/etc/permissions/qti-vzw-ims-internal.xml
-sed -i "s|/system/vendor/framework/qti-vzw-ims-internal.jar|/vendor/framework/qti-vzw-ims-internal.jar|g" "$QTI_VZW_IMS_INTERNAL"
-
-#
-# Correct qcrilhook library location
-#
-QCRILHOOK="$COMMON_BLOB_ROOT"/vendor/etc/permissions/qcrilhook.xml
-sed -i "s|/system/framework/qcrilhook.jar|/vendor/framework/qcrilhook.jar|g" "$QCRILHOOK"
-
-#
-# Correct android.hidl.manager@1.0-java jar name
-#
-QTI_LIBPERMISSIONS="$COMMON_BLOB_ROOT"/vendor/etc/permissions/qti_libpermissions.xml
-sed -i "s|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g" "$QTI_LIBPERMISSIONS"
-
-#
 # Replace libicuuc.so with libicuuc-v27.so for libMiCameraHal.so
 #
 ICUUC_V27="$COMMON_BLOB_ROOT"/vendor/lib/libicuuc-v27.so
