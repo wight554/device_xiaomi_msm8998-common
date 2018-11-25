82f3024d8bee766d76c6a42dd8cfddea0221561d
Vol Zhdanov <wight554@gmail.com>
2018-11-25 15:59:39 +0000

    msm8998-common: remove system android.hidl.{base,manager}@1.0
    * we aren't gonna switch to versioned vndk anytime soon

--- msm8998.mk
+++ msm8998.mk
@@ -194,9 +194,7 @@ DEVICE_FRAMEWORK_MANIFEST_FILE += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0
 
 # IRQ
 PRODUCT_COPY_FILES += \
