d20306fcac9b36279dfc32780af03a434ee2ba11
Vol Zhdanov <wight554@gmail.com>
2018-12-26 17:22:31 +0000

    msm8998-common: gps: specify qti gnss impl bitness

--- msm8998.mk
+++ msm8998.mk
@@ -181,7 +181,7 @@ PRODUCT_PACKAGES += \
     libgnsspps \
 
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl-qti \
+    android.hardware.gnss@1.0-impl-qti:64 \
     android.hardware.gnss@1.0-service-qti \
 
 PRODUCT_COPY_FILES += \
