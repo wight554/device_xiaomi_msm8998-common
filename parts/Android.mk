2c25eeaf13e2c7879ff8414523f604f361fe4297
Vol Zhdanov <wight554@gmail.com>
2019-01-16 20:15:28 +0000

    msm8998-common: parts: cleanup and organize makefile

--- parts/Android.mk
+++ parts/Android.mk
@@ -1,19 +1,18 @@
 LOCAL_PATH := $(call my-dir)
-ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),chiron sagit))
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
 LOCAL_PACKAGE_NAME := XiaomiParts
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
-LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
-LOCAL_RESOURCE_DIR := $(LOCAL_RESOURCE_DIR)
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res
 
 include $(BUILD_PACKAGE)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
