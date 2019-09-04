b3db3496336db800865c09f6644af651f4230909
Volodymyr Zhdanov <wight554@gmail.com>
2019-09-04 17:44:21 +0300

    [DNM]: msm8998-common: permisive for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,6 +55,7 @@ BOARD_KERNEL_CMDLINE := androidboot.console=ttyMSM0 androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive androidboot.veritymode=eio
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8998
@@ -208,6 +209,7 @@ include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # System as root
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
